09816dd3800a3686f2e203ba90df86d06b9b2c0f
shashi <shashiweb64@gmail.com>
2026-05-02 15:15:35 +0530

    change

--- src/application/service/Apiservice.js
+++ src/application/service/Apiservice.js
@@ -1,7 +1,7 @@
 // live url
-export const baseurl = "https://final-backend-3unz.onrender.com";
+// export const baseurl = "https://final-backend-3unz.onrender.com";
 
 
 // local url
-// export const baseurl = "http://localhost:9700";
+export const baseurl = "http://localhost:9700";
 
